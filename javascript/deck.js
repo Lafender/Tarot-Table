757fc232e2398eb8cad96bf05f8395a03618d437
Daryll <11316682+Lafender@users.noreply.github.com>
2018-10-20 12:48:33 -0400

    Update deck.js
    updated comments

--- javascript/deck.js
+++ javascript/deck.js
@@ -3,9 +3,10 @@ if you are adding your own cards:
 set var cardlist array with the names of your cards 
 (make sure they are in quotes so that they are string variables)
 
-all of the cards in the deck are called by their location in the array.
+all of the cards in the deck are called by their integer location in the array.
 this is done so that they can be drawn randomly by the Math.random() function
 */
+
 var cardlist = ["00fool.png", "01magician.png", "02priestess.png", "03empress.png", "04emperor.png", "05heirophant.png", "06lovers.png", "07chariot.png", "08justice.png", "09hermit.png", "10wheeloffortune.png", "11strength.png", "12hangedman.png", "13death.png", "14temperance.png", "15devil.png", "16tower.png", "17star.png", "18moon.png", "19sun.png", "20judgment.png", "21world.png", "coins01.png", "coins02.png", "coins03.png", "coins04.png", "coins05.png", "coins06.png", "coins07.png", "coins08.png", "coins09.png", "coins10.png", "coinsking.png", "coinsknight.png", "coinspage.png", "coinsqueen.png", "cups01.png", "cups02.png", "cups03.png", "cups04.png", "cups05.png", "cups06.png", "cups07.png", "cups08.png", "cups09.png", "cups10.png", "cupsking.png", "cupsknight.png", "cupspage.png", "cupsqueen.png", "swords01.png", "swords02.png", "swords03.png", "swords04.png", "swords05.png", "swords06.png", "swords07.png", "swords08.png", "swords09.png", "swords10.png", "swordsking.png", "swordsknight.png", "swordspage.png", "swordsqueen.png", "wands01.png", "wands02.png", "wands03.png", "wands04.png", "wands05.png", "wands06.png", "wands07.png", "wands08.png", "wands09.png", "wands10.png", "wandsking.png", "wandsknight.png", "wandspage.png", "wandsqueen.png"];
 
 // define newdeck variable as the size of your deck
@@ -14,18 +15,24 @@ var newdeck = 79; // 79 is the standard number of cards in a tarot deck
 
 /*
 the drawn array will record which cards have been drawn so that they are not drawn twice.
-it will have its length checked and will be cleared when its length is equal to newdeck variable
+it will have its length checked and will be cleared when its length is equal to or greater than the newdeck variable
 */
-var drawn = [newdeck]; // newdeck is an integer. 
+var drawn = [newdeck]; // here newdeck can be replaced with any integer.
 
-
-var test_interval;
+var test_interval; 
 
 
 function showcard(x, img) {
+	// x is the integer which selects where on the table the card will be placed
+	// img is the integer that selects a card name from the cardlist array
 	document.getElementById(cards[x]).src = "tarot/" + cardlist[img];
+	// warning: there is no "cards" array in "deck.html" 
 };
 
+/*
+the draw() funtion is called when a card image is clicked
+findcard is passed to showcard(x) and is based on the id (from the html doc) of which card was clicked.
+*/
 function draw(findcard) {
 	var setcard = Math.floor(Math.random() * 78);
 	if (drawn.includes(setcard) != true) {
@@ -39,6 +46,13 @@ function draw(findcard) {
 		draw(findcard);
 	};
 };
+
+
+
+/*
+functions below this comment are debugging commands.
+use them to check if all your card images are loading correctly
+*/
 
 function reset_deck() {
 	drawn = [newdeck];
